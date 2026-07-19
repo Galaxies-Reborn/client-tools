0d2d3846df3a02bb000736935e561014f60599e8
Sais <github@swgsais.com>
2026-07-19 14:50:20 -0400

    feat: add Dancer master client acceptance

--- src/game/client/application/SwgClient/src/win32/ClientMain.cpp
+++ src/game/client/application/SwgClient/src/win32/ClientMain.cpp
@@ -216,11 +216,12 @@ namespace ClientMainNamespace
 		BIC_surrenderDancerKnowledgeOne,
 		BIC_surrenderDancerKnowledgeTwo,
 		BIC_surrenderDancerKnowledgeThree,
-		BIC_surrenderDancerKnowledgeFour
+		BIC_surrenderDancerKnowledgeFour,
+		BIC_surrenderDancerMaster
 	};
 
 	char const * const cms_backgroundInputMessageName = "SWGSource.PreCU.BackgroundInput.v1";
-	LRESULT const cms_backgroundInputProtocolVersion = 64;
+	LRESULT const cms_backgroundInputProtocolVersion = 65;
 	LRESULT const cms_backgroundCombatQueueStatusMarker = 0x43510000;
 	LRESULT const cms_backgroundCombatQueueStatusInCombat = 0x00008000;
 	LRESULT const cms_backgroundCombatQueueStatusHasTarget = 0x00004000;
@@ -1284,6 +1285,23 @@ namespace ClientMainNamespace
 		return queued;
 	}
 
+	bool performBackgroundSurrenderDancerMaster()
+	{
+		Object * const player = Game::getPlayer();
+		if (!player ||
+			Game::getPlayerNetworkId().getValueString() != "39008597")
+			return false;
+
+		ClientCommandQueue::clearLastCommandRemoval();
+		ClientCommandQueue::commandsAreNowFromToolbar(true);
+		bool const queued = ClientCommandQueue::enqueueCommand(
+			"surrenderSkill",
+			NetworkId::cms_invalid,
+			Unicode::narrowToWide("social_dancer_master")) != 0;
+		ClientCommandQueue::commandsAreNowFromToolbar(false);
+		return queued;
+	}
+
 	LRESULT performBackgroundSelectCloneLocation(
 		LPARAM const selectionIndex, bool const confirm)
 	{
@@ -1853,6 +1871,11 @@ namespace ClientMainNamespace
 
 			case BIC_surrenderDancerKnowledgeFour:
 				return performBackgroundSurrenderDancerKnowledgeFour()
+					? 1
+					: 0;
+
+			case BIC_surrenderDancerMaster:
+				return performBackgroundSurrenderDancerMaster()
 					? 1
 					: 0;
 
