79c0ba3d17ca4543676ab185c9a69dc33b1e4b8d
Sais <github@swgsais.com>
2026-07-19 21:37:21 -0400

    feat: add Musician Shock III client acceptance

--- src/game/client/application/SwgClient/src/win32/ClientMain.cpp
+++ src/game/client/application/SwgClient/src/win32/ClientMain.cpp
@@ -228,11 +228,12 @@ namespace ClientMainNamespace
 		BIC_surrenderMusicianWoundThree,
 		BIC_surrenderMusicianWoundFour,
 		BIC_surrenderMusicianShockOne,
-		BIC_surrenderMusicianShockTwo
+		BIC_surrenderMusicianShockTwo,
+		BIC_surrenderMusicianShockThree
 	};
 
 	char const * const cms_backgroundInputMessageName = "SWGSource.PreCU.BackgroundInput.v1";
-	LRESULT const cms_backgroundInputProtocolVersion = 76;
+	LRESULT const cms_backgroundInputProtocolVersion = 77;
 	LRESULT const cms_backgroundCombatQueueStatusMarker = 0x43510000;
 	LRESULT const cms_backgroundCombatQueueStatusInCombat = 0x00008000;
 	LRESULT const cms_backgroundCombatQueueStatusHasTarget = 0x00004000;
@@ -1493,6 +1494,22 @@ namespace ClientMainNamespace
 		return queued;
 	}
 
+	bool performBackgroundSurrenderMusicianShockThree()
+	{
+		Object * const player = Game::getPlayer();
+		if (!player ||
+			Game::getPlayerNetworkId().getValueString() != "39008597")
+			return false;
+		ClientCommandQueue::clearLastCommandRemoval();
+		ClientCommandQueue::commandsAreNowFromToolbar(true);
+		bool const queued = ClientCommandQueue::enqueueCommand(
+			"surrenderSkill",
+			NetworkId::cms_invalid,
+			Unicode::narrowToWide("social_musician_shock_03")) != 0;
+		ClientCommandQueue::commandsAreNowFromToolbar(false);
+		return queued;
+	}
+
 	LRESULT performBackgroundSelectCloneLocation(
 		LPARAM const selectionIndex, bool const confirm)
 	{
@@ -2122,6 +2139,11 @@ namespace ClientMainNamespace
 
 			case BIC_surrenderMusicianShockTwo:
 				return performBackgroundSurrenderMusicianShockTwo()
+					? 1
+					: 0;
+
+			case BIC_surrenderMusicianShockThree:
+				return performBackgroundSurrenderMusicianShockThree()
 					? 1
 					: 0;
 
