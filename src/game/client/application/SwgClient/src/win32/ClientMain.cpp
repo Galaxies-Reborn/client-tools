b9e4036723dab1ffc1c57b60e2d28100f646053d
Sais <github@swgsais.com>
2026-07-26 12:16:34 -0400

    Expose polearmLunge1 background bridge

--- src/game/client/application/SwgClient/src/win32/ClientMain.cpp
+++ src/game/client/application/SwgClient/src/win32/ClientMain.cpp
@@ -454,11 +454,13 @@ namespace ClientMainNamespace
 		BIC_queueFlushingShot1,
 		BIC_flushingShot1WeaponStatus,
 		BIC_queueFlushingShot2,
-		BIC_flushingShot2WeaponStatus
+		BIC_flushingShot2WeaponStatus,
+		BIC_queuePolearmLunge1,
+		BIC_polearmLunge1WeaponStatus
 	};
 
 	char const * const cms_backgroundInputMessageName = "SWGSource.PreCU.BackgroundInput.v1";
-	LRESULT const cms_backgroundInputProtocolVersion = 206;
+	LRESULT const cms_backgroundInputProtocolVersion = 207;
 	LRESULT const cms_backgroundSkillsStatusMarker = 0x534b0000;
 	LRESULT const cms_backgroundSkillsSelectionMarker = 0x53500000;
 	LRESULT const cms_backgroundCombatQueueStatusMarker = 0x43510000;
@@ -3947,6 +3949,17 @@ namespace ClientMainNamespace
 			case BIC_flushingShot2WeaponStatus:
 				return getBackgroundGeneratedCombatWeaponStatus(
 					"flushingShot2");
+
+			case BIC_queuePolearmLunge1:
+				if (lParam < 1 || lParam > 16 ||
+					!performBackgroundQueueMarksmanTier1(
+						"polearmLunge1", static_cast<int>(lParam)))
+					return 0;
+				return getBackgroundCombatQueueStatus();
+
+			case BIC_polearmLunge1WeaponStatus:
+				return getBackgroundGeneratedCombatWeaponStatus(
+					"polearmLunge1");
 
 			default:
 				return 0;
